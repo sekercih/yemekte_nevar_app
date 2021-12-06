bb16c86148101b08ed1749931ff36c8fc928a745
sekercih <hkkskrc@gmail.com>
2021-12-06 23:51:29 +0300

    start

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: use_key_in_widget_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.amber);
+  }
+}
