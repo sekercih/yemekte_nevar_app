c6f13c77d86c2f88e13b92b7ddc41dac983cc63d
sekercih <hkkskrc@gmail.com>
2021-12-07 19:53:43 +0300

    Expande

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: use_key_in_widget_constructors
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
 
 import 'package:flutter/material.dart';
 
@@ -9,6 +9,34 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.amber);
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          centerTitle: true,
+          title: Text(
+            "Bugun Yemekte Ne Var",
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+        body: YemekSayfasi(),
+      ),
+    );
+  }
+}
+
+class YemekSayfasi extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Expanded(flex: 1, child: Image.asset("assets/corba_1.jpg")),
+          Expanded(flex: 1, child: Image.asset("assets/yemek_1.jpg")),
+          Expanded(flex: 1, child: Image.asset("assets/tatli_1.jpg")),
+        ],
+      ),
+    );
   }
 }
